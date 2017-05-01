073e1cfbe26f5c8d86997852e4c292f5a14dac3f
Felipe Perez <phillip.perez44@gmail.com>
2017-04-30 22:58:45 -0700

    done with 18

--- app/scripts/src/storage.js
+++ app/scripts/src/storage.js
@@ -1,19 +1,19 @@
 class Store {
-  constructor(storageApi) {
-    this.api = storageApi;
-  }
+    constructor(storageApi) {
+        this.api = storageApi;
+    }
 
-  get() {
-    return this.api.getItem(this.key);
-  }
-  set(value) {
-    this.api.setItem(this.key, value);
-  }
+    get() {
+        return this.api.getItem(this.key);
+    }
+    set(value) {
+        this.api.setItem(this.key, value);
+    }
 }
 
 export class UserStore extends Store {
-  constructor(key) {
-    super(sessionStorage);
-    this.key = key;
-  }
+    constructor(key) {
+        super(sessionStorage);
+        this.key = key;
+    }
 }
--- extract.js
+++ extract.js
@@ -1,16 +1,12 @@
 var path = require('path');
-
-var extractPath = function (url) {
-  var filePath;
-  var fileName = 'index.html';
-
-  if (url.length > 1) {
-    fileName = url.substring(1);
-  }
-  console.log('The fileName is: ' + fileName);
-
-  filePath = path.resolve(__dirname, 'app', fileName);
-  return filePath;
+var extractFilePath = function(url) {
+    var filePath;
+    var fileName = 'index.html';
+    if (url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log('The fileName is: ' + fileName);
+    filePath = path.resolve(__dirname, 'app', fileName);
+    return filePath;
 };
-
-module.exports = extractPath;
+module.exports = extractFilePath;
